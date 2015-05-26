f97e225dade5acd419de3988ae87c6f40615a5d8
贺思佳 <atommon@sina.com>
2015-05-26 22:06:14 +0800

    waterballcontrol
    update: make the waterball rise and fall

--- Assets/Script/Scene1/WaterballControl.cs
+++ Assets/Script/Scene1/WaterballControl.cs
@@ -1,15 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+
 public class WaterballControl : MonoBehaviour {
+
 	public float rotatespeed = 1;
+	public GameObject waterball;
+	public ParticleSystem rise_water;
+
+	private float cur_r;
+	public const float MAXRADIUS = 50.0f;
+	public const float EXPANDSPEED = 0.2f;
+	private short expand;	//expand or shrink
+
 	// Use this for initialization
 	void Start () {
-	
+		rise_water.enableEmission = false;
+		waterball.SetActive (false);
+		cur_r = MAXRADIUS;
+		expand = -1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (new Vector3 (0, rotatespeed, 0));
+
+		// Activate expand state
+		if (Input.GetKeyDown(KeyCode.A)) {
+			Debug.Log("Input:A");
+			float time = waterball.transform.position.y / rise_water.startSpeed;
+			Invoke ("activeExpand",time);
+			rise_water.enableEmission = true;
+		}
+		//Activate shrink state
+		if (Input.GetKeyDown(KeyCode.S)) {
+			Debug.Log("Input.S");
+			expand = 0;
+			rise_water.enableEmission = false;
+		}
+
+		//process
+		if (expand > 0) {	//expand the ball
+				if (cur_r < MAXRADIUS) {	//animation
+					waterball.transform.localScale += new Vector3 (EXPANDSPEED, EXPANDSPEED, EXPANDSPEED);
+					cur_r += EXPANDSPEED;
+				}
+				else{
+					rise_water.enableEmission = false;
+				}
+		} else if(expand == 0) {	//shrink to nothing
+				if (cur_r > 0 ) {			//animation
+					waterball.transform.localScale -= new Vector3 (EXPANDSPEED, EXPANDSPEED, EXPANDSPEED);
+					cur_r -= EXPANDSPEED;
+				}
+				else{
+					waterball.SetActive (false);
+					expand = -1;
+				}
+		}
+
+		waterball.transform.Rotate (new Vector3 (0, rotatespeed, 0));
 	}
+
+	//Update the states to enable expand
+	void activeExpand()
+	{
+		waterball.transform.localScale = new Vector3(0.0f,0.0f,0.0f);
+		waterball.SetActive (true);
+		cur_r = 0;
+		expand = 1;
+	}
+
 }
